7b19715fc9011aa0920c97c3314ce78a885269f2
Zuzanna_Sosnowska <126651779+prog-zuzanna-sosnowska@users.noreply.github.com>
2024-03-09 12:08:44 +0100

    Delete Zadanie_6.py

--- Zadanie_6.py
+++
@@ -1,8 +0,0 @@
-symbol = "*"
-
-for i in range(5):
-    for j in range(i):
-        print(symbol, end="")
-    print()
-
-print("\n\n  *  \n *** \n*****\n *** \n*****\n *** \n*****\n  *  \n  *  \n")
